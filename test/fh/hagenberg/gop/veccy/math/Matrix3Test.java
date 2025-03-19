a13e6e30767b5b215f669409de418176d2b324db
Roman Groiss <36261833+rgroiss@users.noreply.github.com>
2025-03-19 14:31:18 +0100

    mini fix

--- test/fh/hagenberg/gop/veccy/math/Matrix3Test.java
+++ test/fh/hagenberg/gop/veccy/math/Matrix3Test.java
@@ -11,15 +11,12 @@ public class Matrix3Test {
 
     @Test
     public void testMult(){
-        Matrix3 m3 = new Matrix3(new double[][]{{1,2,3},{4,5,6},{7,8,9}});
 
+        Matrix3 m3 = new Matrix3(new double[][]{{1,2,3},{4,5,6},{7,8,9}});
         Vector3 v1 = new Vector3(new double[]{1,2,3});
 
-        Matrix3 emptyM = new Matrix3();
         Matrix3 r1 = m3.mult(m3);
-
-        Vector3 emptyV = new Vector3();
-        Vector3 r2 = m3.mult(v1);//possible
+        Vector3 r2 = m3.mult(v1);
 
 
         assertArrayEquals(new double[][]{{30,36,42},{66,81,96},{102,126,150}}, r1.getValues());
